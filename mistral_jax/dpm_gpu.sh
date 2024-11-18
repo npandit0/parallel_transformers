5d51200a306d188b15875b208c7b260c3a9e6133
Xavier Gonzalez <xavier18@stanford.edu>
2024-11-17 20:41:48 -0800

    minor (quick-push)

--- mistral_jax/dpm_gpu.sh
+++ mistral_jax/dpm_gpu.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=dpm_gpu
 #SBATCH --error=dpm_gpu_%j_%a.err
 #SBATCH --out=dpm_gpu_%j_%a.out
-#SBATCH --time=01:59:59
+#SBATCH --time=23:59:59
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=4
 #SBATCH --mail-type=ALL
