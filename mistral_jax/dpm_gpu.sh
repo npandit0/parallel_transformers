2405466b745b6c075f920480a175ecac0ce21a3b
Xavier Gonzalez <xavier18@stanford.edu>
2024-12-08 08:36:11 -0800

    minor (quick-push)

--- mistral_jax/dpm_gpu.sh
+++ mistral_jax/dpm_gpu.sh
@@ -7,4 +7,4 @@
 #SBATCH --cpus-per-gpu=4
 #SBATCH --mail-type=ALL
 
-$SCRATCH/parallel_transformers/venvs/mistral_jax/bin/python deer_prototype_mistral.py $@ --xavier
+$SCRATCH/parallel_transformers/venvs/mistral_jax/bin/python deer_prototype_mistral.py $@
