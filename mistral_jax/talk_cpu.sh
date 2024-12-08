ce3dd65557cec2a8ac2b4fbdccb793981432184b
Xavier Gonzalez <xavier18@stanford.edu>
2024-12-08 09:31:27 -0800

    Shell file to put talk on bigmem

---
+++ mistral_jax/talk_cpu.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+#SBATCH --job-name=talk_cpu
+#SBATCH --error=talk_cpu_%j_%a.err
+#SBATCH --out=talk_cpu_%j_%a.out
+#SBATCH --time=23:59:59
+#SBATCH --mail-type=ALL
+#SBATCH --partition=bigmem
+#SBATCH --mem=256G  # minimum for bigmem
+
+export JAX_PLATFORM_NAME=cpu
+$SCRATCH/parallel_transformers/venvs/mistral_jax/bin/python the_model_speaks.py $@ --load_weights
